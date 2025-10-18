5e31e0f530551c057551f8c8f053308bc10e5d53
KrasiG <krasi@indexbg.bg>
2025-10-18 17:09:57 +0300

    finish

--- src/components/survey/LearningSurvey.vue
+++ src/components/survey/LearningSurvey.vue
@@ -13,22 +13,15 @@
           <label for="rating-poor">Poor</label>
         </div>
         <div class="form-control">
-          <input
-            type="radio"
-            id="rating-average"
-            value="average"
-            name="rating"
-            v-model="chosenRating"
-          />
+          <input type="radio" id="rating-average" value="average" name="rating" v-model="chosenRating" />
           <label for="rating-average">Average</label>
         </div>
         <div class="form-control">
           <input type="radio" id="rating-great" value="great" name="rating" v-model="chosenRating" />
           <label for="rating-great">Great</label>
         </div>
-        <p
-          v-if="invalidInput"
-        >One or more input fields are invalid. Please check your provided data.</p>
+        <p v-if="invalidInput">One or more input fields are invalid. Please check your provided data.</p>
+        <p v-if="error">Error submiting data</p>
         <div>
           <base-button>Submit</base-button>
         </div>
@@ -44,9 +37,10 @@ export default {
       enteredName: '',
       chosenRating: null,
       invalidInput: false,
+      error:null,
     };
   },
-  emits: ['survey-submit'],
+  //emits: ['survey-submit'],
   methods: {
     submitSurvey() {
       if (this.enteredName === '' || !this.chosenRating) {
@@ -55,10 +49,29 @@ export default {
       }
       this.invalidInput = false;
 
-      this.$emit('survey-submit', {
-        userName: this.enteredName,
-        rating: this.chosenRating,
-      });
+      // this.$emit('survey-submit', {
+      //   userName: this.enteredName,
+      //   rating: this.chosenRating,
+      // });
+
+      fetch('https://vute-tests-default-rtdb.europe-west1.firebasedatabase.app/surveys.json', {
+        method: 'POST',
+        body: JSON.stringify({
+          name: this.enteredName,
+          rating: this.chosenRating,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }).then((response)=>{
+        if (!response.ok) {
+          throw new Error('Failed to submit survey.');
+        }
+        return response.json();
+      }).catch((error)=>{
+          console.log(error);
+          this.error=error.message;
+        });
 
       this.enteredName = '';
       this.chosenRating = null;
--- src/components/survey/UserExperiences.vue
+++ src/components/survey/UserExperiences.vue
@@ -3,15 +3,15 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button @click="loadExperiances">Load Submitted Experiences</base-button>
       </div>
-      <ul>
-        <survey-result
-          v-for="result in results"
-          :key="result.id"
-          :name="result.name"
-          :rating="result.rating"
-        ></survey-result>
+      <p v-if="isLoading">Loading ...</p>
+      <p v-else-if="!isLoading && !error && (!results || results.length === 0)">No submitted experiences found.</p>
+      <p v-else-if="!isLoading && error">{{error}}</p>
+      <ul v-else-if="!isLoading && results && results.length > 0">
+        
+        <survey-result v-for="result in results" :key="result.id" :name="result.name"
+          :rating="result.rating"></survey-result>
       </ul>
     </base-card>
   </section>
@@ -21,9 +21,50 @@
 import SurveyResult from './SurveyResult.vue';
 
 export default {
-  props: ['results'],
+  
   components: {
     SurveyResult,
+  },
+  data() {
+    return {
+      results: [],
+      isLoading:false,
+      error:null,
+    };
+  },
+  methods: {
+    loadExperiances() {
+      this.isLoading = true;
+      this.error = null;
+      fetch('https://vute-tests-default-rtdb.europe-west1.firebasedatabase.app/surveys.json')
+        .then((response)=>{
+          if (response.ok) {
+            return response.json();
+          }
+        })
+        .then((data) => {
+          console.log(data);
+          this.isLoading = false;
+          const surveyResults = [];
+          for(const id in data){
+            surveyResults.push({
+              id: id,
+              name: data[id].name,
+              rating: data[id].rating,
+            });
+          }
+          this.results = surveyResults;
+        })
+        .catch((error)=>{
+          console.log(error);
+          this.isLoading = false;
+          this.error = 'Failed to fetch data. Please try again later.';
+        });
+        
+    },
+  },
+  mounted() {
+    this.loadExperiances(); 
   },
 };
 </script>
